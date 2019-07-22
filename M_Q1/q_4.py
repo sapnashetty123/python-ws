6c5bbae2fb2b6f9c2e663cc04205b6551d09cb66
unknown <sapnabshetty2@gmail.com>
2019-07-22 15:24:12 +0530

    sum of cube

--- M_Q1/q_4.py
+++ M_Q1/q_4.py
@@ -1,9 +1,11 @@
-num = int(input("Enter the number :"))
-temp = num
-rev = 0
-
-while num != 0:
-    rev = rev * 10 num % 10
-    num //= 10
-
-print(f"Reverse of {temp}is {rev}")
+'''4.	Write a program to accept a number “n” from the user; then display the sum of the following series:
+1/23+1/33+1/43 + ……+1/n3'''
+'''def cube_of(num):
+    cube = num * num * num
+    return cube'''
+num=int(input("Enter the number: "))
+sum=0
+while num != 1:
+    sum = sum + ( 1 / (num * num * num) )
+    num = num - 1
+print(f"the sum of the series is {sum}")
