e1fbdec1e2195925cd8dcc29f984a27f2111d491
unknown <sapnabshetty2@gmail.com>
2019-07-22 16:17:37 +0530

    exponent

---
+++ M_Q1/q_8.py
@@ -0,0 +1,4 @@
+'''8.	Write a program to accept 2 numbers “m” and “n” from the user; determine the value of mn without using predefined functions.'''
+num1 = int(input("Enter first number:"))
+num2 = int(input("Enter second number:"))
+print(num1 ** num2)
