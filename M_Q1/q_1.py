8b913049844169a27731944e2d6178c01e5c0c45
unknown <sapnabshetty2@gmail.com>
2019-07-24 14:55:45 +0530

    prime

--- M_Q1/q_1.py
+++ M_Q1/q_1.py
@@ -1,12 +1,15 @@
 '''1.	Write a program to accept a number and determine whether it is a prime number or not.'''
-num=int(input("enter the number:"))
-if num < 2:
-    print(f"{num} is not prime")
-else:
-    for i in range (2,num // 2 +1):
+
+def is_prime(num):
+    if num < 2:
+        return False
+    for i in range(2,num//2+1):
         if num % i == 0:
-            print(f"{num} is not a prime no")
-            break
-    else:
-        print(f"{num} is prime")
+            return False
+    return True
+num=int(input("enter the number:"))
+if is_prime(num):
+    print(f"{num} is prime")
+else:
+    print(f"{num} is not prime")
         
