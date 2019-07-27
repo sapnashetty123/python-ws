f060d81b23277bf0519c63bef136bf91df22e977
unknown <sapnabshetty2@gmail.com>
2019-07-27 12:27:58 +0530

    file excep

---
+++ exceptions/transtofile.py
@@ -0,0 +1,7 @@
+try :
+    with open("data.txt") as fin ,open("u_data.txt","w") as fout:
+        lines = fin.readlines()
+        lines = [l.upper() for l in lines]
+        fout.writelines(lines)
+except Exception as e:
+    print(f"File not found plz check path {e}")
